79a7d63cb7bed5f5f62c1775446d491688984bce
spamtoon <lourencoibaviva@gmail.com>
2022-01-09 16:40:35 +0000

    Python

--- Python/input.py
+++ Python/input.py
@@ -1,2 +1,6 @@
 num1 = int(input('Type the first number: '))
-num2 - int(input(''))
+num2 = int(input('Type the second one: '))
+
+result = num1 + num2
+
+print(f'The result of {num1} and {num2} is {result}')
