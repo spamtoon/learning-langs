f161453f33be4df6c3e12d3a68420dc728c1bd80
spamtoon <lourencoibaviva@gmail.com>
2022-01-11 21:56:04 +0000

    inputs.hello

--- C/inputs.c
+++ C/inputs.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
 int main(){
-    int num1 = getd();
-    int num2 = getd();
-    int res = num1 + num2;
+    int num1;
+    int num2;
+    int res;
 
+    printf("Type some number: ");
+    scanf("%d", &num1);
+    printf("Type the second one: ");
+    scanf("%d", &num2);
+    
+    res = num1 + num2;
     printf("The result is: %d \n", res);
     
     return 0;
